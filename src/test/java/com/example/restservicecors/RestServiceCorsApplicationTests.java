478732e159253bb2dfa3f07a663b0ed115023a73
Maho Takara <takara@labo.local>
2022-04-29 20:46:55 +0900

    fix

--- src/test/java/com/example/restservicecors/RestServiceCorsApplicationTests.java
+++ src/test/java/com/example/restservicecors/RestServiceCorsApplicationTests.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 //@WebMvcTest(controllers = GreetingController.class)
 //@SpringBootTest
-@WebMvcTest
+@WebMvcTest(controllers = UserController.class)
 public class RestServiceCorsApplicationTests {
 
     @Autowired

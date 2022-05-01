bef5954d242947b1567c3f582f73d03d716bdf63
Maho Takara <takara@labo.local>
2022-05-01 21:11:40 +0900

    削除じゃないのか

--- src/main/resources/public/hello.js
+++
@@ -1,8 +0,0 @@
-angular.module('demo', [])
-    .controller('Hello', function($scope, $http) {
-
-    $http.get('/greeting').
-        then(function(response) {
-            $scope.greeting = response.data;
-        });
-});

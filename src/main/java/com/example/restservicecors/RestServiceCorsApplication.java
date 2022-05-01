0cfb1ca9dc241cff83d816336827233d510ef26b
Maho Takara <takara@labo.local>
2022-05-01 13:40:38 +0900

    リファクタ

--- src/main/java/com/example/restservicecors/RestServiceCorsApplication.java
+++ src/main/java/com/example/restservicecors/RestServiceCorsApplication.java
@@ -1,10 +1,7 @@
 package com.example.restservicecors;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestServiceCorsApplication {

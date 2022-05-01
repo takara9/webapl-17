1bcd3d35cf6eae411c0e73f15476ec448ef75128
Maho Takara <takara@labo.local>
2022-05-01 13:38:43 +0900

    不要なインポートを削除

--- src/main/java/com/example/restservicecors/RestServiceCorsApplication.java
+++ src/main/java/com/example/restservicecors/RestServiceCorsApplication.java
@@ -2,9 +2,9 @@ package com.example.restservicecors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestServiceCorsApplication {
@@ -13,15 +13,4 @@ public class RestServiceCorsApplication {
 		SpringApplication.run(RestServiceCorsApplication.class, args);
 	}
 
-	/*
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-			    registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080");
-			}
-		};
-	}
-	*/
 }

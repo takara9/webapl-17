51991e675024a2e9736652c4c7550911fcaab29c
Maho Takara <takara@labo.local>
2022-05-04 12:31:48 +0900

    chrome options for insecure

--- selenium-test/src/test/java/com/example/visitorsbook/ChromeWebAccessTest.java
+++ selenium-test/src/test/java/com/example/visitorsbook/ChromeWebAccessTest.java
@@ -11,12 +11,14 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static io.github.bonigarcia.wdm.WebDriverManager.isDockerAvailable;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assumptions.assumeThat;
+
 
 
 public class ChromeWebAccessTest {
